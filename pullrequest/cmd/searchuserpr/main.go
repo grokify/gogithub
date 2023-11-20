cd3d6dc70fd8eca28bab3c31c66077191ce35d5d
John Wang <johncwang@gmail.com>
2023-11-19 17:07:15 -0800

    feat: `cmd/searchuesrpr`: update to add github user/org as multi cli param

--- pullrequest/cmd/searchuserpr/main.go
+++ pullrequest/cmd/searchuserpr/main.go
@@ -3,21 +3,51 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 
+	"github.com/grokify/gocharts/v2/data/table"
 	"github.com/grokify/gogithub"
-	"github.com/grokify/mogo/fmt/fmtutil"
 	"github.com/grokify/mogo/log/logutil"
+	flags "github.com/jessevdk/go-flags"
 )
 
-func main() {
-	c := gogithub.NewClient(nil)
-	r1, _, err := c.SearchOpenPullRequests(context.Background(), "grokify", nil)
-	logutil.FatalErr(err)
-	fmtutil.MustPrintJSON(r1)
+type Options struct {
+	Accounts []string `short:"a" long:"accounts" description:"Accounts" required:"true"`
+}
 
-	iss := gogithub.Issues(r1.Issues)
-	tbl, err := iss.Table()
-	logutil.FatalErr(err)
+func main() {
+	opts := Options{}
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("loading public pull requests for (%s)\n", strings.Join(opts.Accounts, ", "))
+
+	if len(opts.Accounts) == 0 {
+		fmt.Println("DONE")
+		os.Exit(0)
+	}
+
+	c := gogithub.NewClient(nil)
+
+	var tbl *table.Table
+
+	for i, acct := range opts.Accounts {
+		iss2, err := c.SearchIssuesAll(context.Background(), gogithub.Query{
+			"user":  acct,
+			"state": "open",
+			"is":    "pr",
+		}, nil)
+		tbl2, err := iss2.Table()
+		logutil.FatalErr(err)
+		if i == 0 {
+			tbl = tbl2
+		} else {
+			tbl.Rows = append(tbl.Rows, tbl2.Rows...)
+		}
+	}
 
 	err = tbl.WriteXLSX("githubissues.xlsx", "issues")
 	logutil.FatalErr(err)
